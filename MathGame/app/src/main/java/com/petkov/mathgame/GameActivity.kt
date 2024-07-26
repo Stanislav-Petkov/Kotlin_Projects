e5aa1e06b742324b2a7c9f6497d74ffd0feb480f
Stanislav-Petkov <stan_10@abv.bg>
2024-07-26 23:25:11 +0300

    Add a game activity design

--- MathGame/app/src/main/java/com/petkov/mathgame/GameActivity.kt
+++ MathGame/app/src/main/java/com/petkov/mathgame/GameActivity.kt
@@ -1,5 +1,6 @@
 package com.petkov.mathgame
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -8,26 +9,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var addition: Button
-    private lateinit var subtraction: Button
-    private lateinit var multi: Button
+class GameActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        addition = findViewById(R.id.buttonAdd)
-        subtraction = findViewById(R.id.buttonSub)
-        multi = findViewById(R.id.buttonMulti)
 
-        addition.setOnClickListener{
-            val intent = Intent(this@MainActivity, GameActivity::class.java)
-            startActivity(intent)
-        }
     }
 }

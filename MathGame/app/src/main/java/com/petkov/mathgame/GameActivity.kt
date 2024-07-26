f42ee52964d0afa642898c6b3275037177928e33
Stanislav-Petkov <stan_10@abv.bg>
2024-07-26 23:51:23 +0300

    Add a game activity logic

--- MathGame/app/src/main/java/com/petkov/mathgame/GameActivity.kt
+++ MathGame/app/src/main/java/com/petkov/mathgame/GameActivity.kt
@@ -4,14 +4,31 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
 
+    lateinit var textScore: TextView
+    lateinit var textLife: TextView
+    lateinit var textTime: TextView
 
+    lateinit var textQuestion: TextView
+    lateinit var editTextAnswer: EditText
+
+    lateinit var buttonOk: Button
+    lateinit var buttonNext: Button
+
+    var correctAnswer = 0
+    var userScore = 0
+    var userLife = 3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,5 +39,52 @@ class GameActivity : AppCompatActivity() {
             insets
         }
 
+        textScore = findViewById(R.id.textViewScore)
+        textLife = findViewById(R.id.textViewLife)
+        textTime = findViewById(R.id.textViewTime)
+        textQuestion = findViewById(R.id.textViewQuestion)
+        editTextAnswer = findViewById(R.id.editTextAnswer)
+        buttonOk = findViewById(R.id.buttonOk)
+        buttonNext = findViewById(R.id.buttonNext)
+
+        gameContinue()
+
+        buttonOk.setOnClickListener {
+
+            val input = editTextAnswer.text.toString()
+            if (input == "") {
+                Toast.makeText(
+                    this,
+                    "Please write an answer or click the next button",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else{
+                val userAnswer = input.toInt()
+
+                if(userAnswer == correctAnswer){
+                    userScore += 10
+                    textQuestion.text = "Congratulations, your answer is correct"
+                    textScore.text = userScore.toString()
+                }else{
+                    userLife--
+                    textQuestion.text = "Sorry, your answer is wrong"
+                    textLife.text = userLife.toString()
+                }
+            }
+        }
+
+        buttonNext.setOnClickListener {
+            gameContinue()
+            editTextAnswer.text.clear()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun gameContinue() {
+        val number1 = Random.nextInt(0, 100)
+        val number2 = Random.nextInt(0, 100)
+        textQuestion.text = "$number1 + $number2"
+
+        correctAnswer = number1 + number2
     }
 }
